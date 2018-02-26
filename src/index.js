379185d8435a0a2f4a8e276daed19bf6cbf14234
4eKoshka <4ekoshka@gmail.com>
2018-02-26 03:32:06 -0800

    my presolution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,66 @@
 class Sorter {
   constructor() {
+	  this.a1=[];	  
     // your implementation
   }
 
   add(element) {
+	  this.a1.push(element);
     // your implementation
   }
 
   at(index) {
+	  return this.a1[index];
     // your implementation
   }
 
   get length() {
+	 
+	  return this.a1.length;
     // your implementation
   }
 
   toArray() {
-    // your implementation
+	 
+   return this.a1;
+   // your implementation
   }
 
   sort(indices) {
-    // your implementation
+	  var st;
+	  var min;
+	 			
+	  for (var i=0; i < indices.length-1; i++) {
+		  min = i;
+		 for(var j= i+1; j < indices.length; j++) {
+			 if (indices[j] < indices[min]) {min = j;}
+			 
+			st = indices[i];
+			indices[i] = indices[min];
+			indices[min] = st; 
+				
+			}
+		}
+		
+		
+		for (i = 0; i < indices.length-1; i++) {
+			min= i;
+			for(j= i+1; j < indices.length; j++) {
+			 if (this.a1[indices[j]] < this.a1[indices[min]]) { min =j;}
+			 
+			st = this.a1[indices[i]];
+			this.a1[indices[i]] = this.a1[indices[min]];
+			this.a1[indices[min]] = st; 
+					
+			}
+	  }
+	  
+	  return this.a1;
+	  // your implementation
   }
+	
+	
+     
 
   setComparator(compareFunction) {
     // your implementation
